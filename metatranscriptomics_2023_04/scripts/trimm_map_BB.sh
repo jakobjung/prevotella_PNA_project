cc3f6dd472b8c856ea88ddafdc8567907e7a996b
Jakob Jung <jakobjung@tutanota.com>
2023-04-27 13:11:10 +0000

    after_mapping

--- metatranscriptomics_2023_04/scripts/trimm_map_BB.sh
+++ metatranscriptomics_2023_04/scripts/trimm_map_BB.sh
@@ -4,23 +4,23 @@ main(){
     # sequences are stored:
     PROJECT=../data
     echo "Start trimming"
-    rename_trim_rna_libs
+    #rename_trim_rna_libs
     echo "Trimming done. Start mapping"
     align_rna_reads_genome
     echo "Finished mapping. Start connecting all tab files"
-    featureCounts -T 5 -t CDS,sRNA -g locus_tag \
-		  -a $PROJECT/reference_sequences/p_copri_wplasmids.gff3 \
-		  -o $PROJECT/rna_align/counttable.txt \
+    featureCounts -T 5 -t CDS -g locus_tag \
+		  -a $PROJECT/reference_sequences/tot_seqs.gff \
+		  -o $PROJECT/rna_align/counttable_bb.txt -M \
 		  $PROJECT/rna_align/*.bam
-    
+   
     featureCounts -T 5 -t rRNA -g locus_tag \
-		  -a $PROJECT/reference_sequences/p_copri_wplasmids.gff3 \
-		  -o $PROJECT/rna_align/rRNA_counttable.txt \
+		  -a $PROJECT/reference_sequences/tot_seqs.gff \
+		  -o $PROJECT/rna_align/rRNA_counttable_bb.txt \
 		  $PROJECT/rna_align/*.bam
     
     featureCounts -T 5 -t tRNA -g locus_tag \
-		  -a $PROJECT/reference_sequences/p_copri_wplasmids.gff3 \
-		  -o $PROJECT/rna_align/tRNA_counttable.txt \
+		  -a $PROJECT/reference_sequences/tot_seqs.gff \
+		  -o $PROJECT/rna_align/tRNA_counttable_bb.txt \
 		  $PROJECT/rna_align/*.bam
 }
 
@@ -30,13 +30,17 @@ rename_trim_rna_libs(){
     do
         echo "$NAME starts trimming nowwwwwww"
         NEWNAME=${NAME##*/}
-        NEWNAME=${NEWNAME%.fastq.gz}_trimmed.fastq.gz
+        NEWNAME=${NEWNAME%.fq.gz}_trimmed.fastq.gz
         echo $NEWNAME
        # bbduk trims low quality bases and removes adapters:
-        bbduk.sh  in=$NAME \
-			     ref=../data/reference_sequences/adapters.fa -Xmx4g t=20\
-			     out=$PROJECT/libs/${NEWNAME} ktrim=r k=23 mink=11\
-			     hdist=1 qtrim=r trimq=10 ftl=12 
+        #bbduk.sh  in=$NAME \
+	#		     ref=../data/reference_sequences/adapters.fa -Xmx4g t=30\
+	#		     out=$PROJECT/libs/${NEWNAME} ktrim=r k=20 mink=4\
+	#		     hdist=1 qtrim=r trimq=10 ftl=12 overwrite=t
+	cutadapt --trim-n --match-read-wildcards -u 16 -n 4 -a AGATCGGAAGAGCACACGTCTG -a AAAAAAAA \
+		 -a GAACTCCAGTCAC -e 0.2 --nextseq-trim 20 -m 15 \
+		 -o $PROJECT/libs/${NEWNAME} $NAME 
+	fastqc $PROJECT/libs/${NEWNAME}
     done
 }
 
@@ -49,9 +53,17 @@ align_rna_reads_genome(){
         NAME=${i##*/}
         NAME=${NAME%_trimmed.fastq.gz}
         echo "Starting mapping for sample: $NAME"
-        salmon quant -i $PROJECT/reference_sequences/metatranscriptome_index -l A -r $i \
-	       --validateMappings -o $DIR/$NAME
-	fastqc $i
+        #salmon quant -i $PROJECT/reference_sequences/metatranscriptome_index -l A -r $i \
+	#       --fldMean 300 --fldSD 300 --validateMappings --writeUnmappedNames --incompatPrior 0.0 --recoverOrphans --softclipOverhangs -o $DIR/$NAME
+	#fastqc $i
+        #bbmap.sh in=$i trimreaddescription=t  t=20 \
+	#		     ref=$PROJECT/reference_sequences/tot_seqs.fasta \
+	#		     k=12 outm=$DIR/$NAME.sam outu=$DIR/${NAME}_unmapped.sam
+	samtools sort -O BAM -@ 40 $DIR/$NAME.sam > $DIR/$NAME.bam
+	echo  "done sorting, start indexing"
+	samtools index $DIR/$NAME.bam
+	echo "done indexing, start bamcoverage"
+	bamCoverage -b $DIR/$NAME.bam -o $DIR/$NAME.bigwig -of bigwig -bs 1
 	#rm $i
     done
 }
